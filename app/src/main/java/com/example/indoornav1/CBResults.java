aae7d5b0cb7417edb4469e5b07d736378be4635e
coder-garvit <garvitrastogi9044@gmail.com>
2019-06-26 13:22:17 +0530

    added recycler view for stores

--- app/src/main/java/com/example/indoornav1/CBResults.java
+++ app/src/main/java/com/example/indoornav1/CBResults.java
@@ -4,16 +4,78 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CBResults extends AppCompatActivity {
+
+    private List<Store> storeList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private StoreAdapter sAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cbresults);
 
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        sAdapter = new StoreAdapter(storeList);
+
+        recyclerView.setHasFixedSize(true);
+
+        // vertical RecyclerView
+        // keep movie_list_row.xml width to `match_parent`
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+
+        // horizontal RecyclerView
+        // keep movie_list_row.xml width to `wrap_content`
+        // RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
+
+        recyclerView.setLayoutManager(mLayoutManager);
+
+        // adding inbuilt divider line
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+
+        // adding custom divider line with padding 16dp
+        // recyclerView.addItemDecoration(new MyDividerItemDecoration(this, LinearLayoutManager.VERTICAL, 16));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        recyclerView.setAdapter(sAdapter);
+
+        // row click listener
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new RecyclerTouchListener.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Store store = storeList.get(position);
+                Toast.makeText(getApplicationContext(), store.getSid() + " is selected!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
+
+        prepareMovieData();
+    }
+
+    /**
+     * Prepares sample data to provide data set to adapter
+     */
+    private void prepareMovieData() {
+        Store store = new Store("s1", "50", "5", "1kg");
+        storeList.add(store);
+
+        sAdapter.notifyDataSetChanged();
     }
 
 }
